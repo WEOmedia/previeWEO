82eba55a8e750d3d969b19f86bc2fe9b8c1773d0
WEO Media <50425214+WEOmedia@users.noreply.github.com>
2020-03-09 20:23:43 -0700

    Delete previeWEO-css-test.js

--- previeWEO-css-test.js
+++
@@ -1,8 +0,0 @@
-function setupStyles() {
-	//create a link element for the page view styleSheets
-	var pageStyle = document.createElement("link");
-	pageStyle.setAttribute("href", "css/bc_page.css");
-	pageStyle.setAttribute("rel", "stylesheet");	
-	//append the link element to the document head
-	document.head.appendChild(pageStyle);
-}
